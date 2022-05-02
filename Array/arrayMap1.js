8eeb89d794052552e9fd95a0fce80512df84fb7b
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-05-02 09:27:32 -0300

    Map#01

---
+++ Array/arrayMap1.js
@@ -0,0 +1,16 @@
+const nums = [1, 2, 3, 4, 5]
+
+// For com propósito
+
+let result = nums.map(function(e) {
+    return e * 2
+})
+
+console.log(result)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinherio = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+result = nums.map(soma10).map(triplo).map(paraDinherio) 
+console.log(result)
